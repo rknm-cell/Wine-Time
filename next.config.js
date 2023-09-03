bc4e495afab6c1b9497bb1208a9d9526660c608f
rknm-cell <racknahm@gmail.com>
2023-09-03 15:10:05 -0400

    changed api routes in next.config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,16 @@
 module.exports = {
-  async rewrites() {
+  async headers() {
     return [
       {
-        source: '@api/:path*',
-        destination: 'http://127.0.0.1:5555/:path*', // Proxy to Backend
-      },
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
     ]
-  },
-}
+  }
+};
